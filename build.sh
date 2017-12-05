916096de563bc557a96897e7a89bc98a52d77239
Stephan Kulla <git.mail@kulla.me>
2017-12-05 21:33:48 +0100

    Add Plugin "DataTables"

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@ mkdir -p out
 # Create main HTML file
 python create_team_report.py > out/index.html
 
-# Get CSS and JS files
+# Get additional files like CSS and JS
 curl 'https://de.serlo.org/favicon.ico' > out/favicon.ico
+
 curl 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css' > out/style.css
+curl 'https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css' >> out/style.css
+
+curl 'https://code.jquery.com/jquery-3.2.1.min.js' > out/script.js
+curl 'https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js' >> out/script.js
