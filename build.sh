03531eddd7f51776096f134559cff896e5c2b41d
Stephan Kulla <git.mail@kulla.me>
2017-12-05 22:40:58 +0100

    Add dashboard layout

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ python create_team_report.py > out/index.html
 curl 'https://de.serlo.org/favicon.ico' > out/favicon.ico
 
 curl 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css' > out/style.css
+curl 'http://getbootstrap.com/docs/4.0/examples/dashboard/dashboard.css' >> out/style.css
 curl 'https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css' >> out/style.css
 
 curl 'https://code.jquery.com/jquery-3.2.1.min.js' > out/script.js
