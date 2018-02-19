2f72764845b25871669917d624f527399434abc3
Stephan Kulla <git.mail@kulla.me>
2018-02-19 19:58:15 +0100

    Design navbar and sidebar (fixes #26)

--- build.sh
+++ build.sh
@@ -10,7 +10,6 @@ python create_team_report.py > out/index.html
 curl -s 'https://de.serlo.org/favicon.ico' > out/favicon.ico
 
 curl -s 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css' > out/style.css
-curl -s 'http://getbootstrap.com/docs/4.0/examples/dashboard/dashboard.css' >> out/style.css
 curl -s 'https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css' >> out/style.css
 
 curl -s 'https://code.jquery.com/jquery-3.2.1.min.js' > out/script.js
