458a4999c9209a5c8e956280c051d5bb11e8cc13
Stephan Kulla <git.mail@kulla.me>
2018-03-15 12:34:03 +0100

    test: Rewrite TestPerson

--- serlo/test/test_model.py
+++ serlo/test/test_model.py
@@ -1,77 +1,8 @@
 """Tests for the modul `serlo.model`."""
 
-from abc import ABC, abstractmethod
 from unittest import TestCase
 
 from serlo.model import Email, Person, PhoneNumber, SerloDatabase
-
-# pylint: disable=missing-docstring
-
-def generate_specs(params, list_values):
-    """Helper function for creating model specifications."""
-    return [dict(zip(params, values)) for values in list_values]
-
-class TestGenericModel(TestCase):
-    """Generic tests for the models."""
-
-    def setUp(self):
-        self.models = [Person]
-
-    def test_attr_tablename(self):
-        """Test: Each model needs to have the attribute `__tablename__`."""
-        for cls in self.models:
-            self.assertEqual(cls.__tablename__, cls.__name__.lower())
-
-class ModelTest(ABC, TestCase):
-    """Base class for tests of a model."""
-
-    @property
-    @abstractmethod
-    def specs(self):
-        """List of specifications of different model objects."""
-        raise NotImplementedError()
-
-    @property
-    @abstractmethod
-    def cls(self):
-        """Class of the model."""
-        raise NotImplementedError()
-
-    def create_entity(self, spec):
-        """Creates an entity of type `self.cls` based on the given
-        specification."""
-        return self.cls(**spec)
-
-    def setUp(self):
-        self.database = SerloDatabase("sqlite:///:memory:")
-
-    def test_model_initialization(self):
-        """Test Initialization of a model with several specs and check whether
-        attributes are set properly."""
-        for spec in self.specs:
-            obj = self.create_entity(spec)
-
-            for name, value in spec.items():
-                self.assertEqual(getattr(obj, name), value)
-
-            # Default value of `id` is None
-            self.assertIsNone(obj.id, None)
-
-    def test_add_objects_to_database(self):
-        """Tests whether objects can be saved inside the database."""
-        objects = [self.create_entity(spec) for spec in self.specs]
-
-        self.database.add_all(objects)
-
-        # pylint: disable=protected-access
-        session = self.database._sessionmaker()
-
-        self.assertSetEqual(set(objects), set(session.query(self.cls)))
-
-        # saved objects need to have an id
-        for obj in objects:
-            self.assertIsNotNone(obj.id)
-            self.assertGreater(obj.id, 0)
 
 class TestEmail(TestCase):
     """Testcases for the model `Email`."""
@@ -121,27 +52,47 @@ class TestPhoneNumber(TestCase):
         self.assertEqual(self.number2.number, "0178645389")
         self.assertEqual(self.number3.number, "")
 
-class TestPerson(ModelTest, TestCase):
+class TestPerson(TestCase):
     """Testcases for model `Person`."""
-    specs = generate_specs(
-        ("first_name", "last_name"),
-        [("Hello", "World"), ("Markus", "Lukas"), ("", "abc")]
-    )
 
-    cls = Person
+    def setUp(self):
+        self.person1 = Person(first_name="Markus", last_name="Miller")
+        self.person2 = Person(first_name="Yannick", last_name="Müller")
+        self.person3 = Person(first_name="", last_name="")
+
+    def test_attribute_id(self):
+        """Testcase for attribute `Person.id`."""
+        self.assertIsNone(self.person1.id)
+        self.assertIsNone(self.person2.id)
+
+    def test_attribute_tablename(self):
+        """Testcase for attribute `Person.__tablename__`."""
+        self.assertEqual(Person.__tablename__, "person")
+
+    def test_attribute_first_name(self):
+        """Testcase for attribute `Person.first_name`."""
+        self.assertEqual(self.person1.first_name, "Markus")
+        self.assertEqual(self.person2.first_name, "Yannick")
+        self.assertEqual(self.person3.first_name, "")
+
+    def test_attribute_last_name(self):
+        """Testcase for attribute `Person.last_name`."""
+        self.assertEqual(self.person1.last_name, "Miller")
+        self.assertEqual(self.person2.last_name, "Müller")
+        self.assertEqual(self.person3.last_name, "")
 
     def test_attribute_name(self):
         """Testcase for attribute `Person.name`."""
-        names = [self.create_entity(spec).name for spec in self.specs]
-
-        self.assertListEqual(names, ["Hello World", "Markus Lukas", " abc"])
+        self.assertEqual(self.person1.name, "Markus Miller")
+        self.assertEqual(self.person2.name, "Yannick Müller")
+        self.assertEqual(self.person3.name, " ")
 
 class TestSerloDatabase(TestCase):
     """Testcases for the class `SerloDatabase`."""
 
+    def setUp(self):
+        self.database = SerloDatabase("sqlite:///:memory:")
+
     def test_serlo_database_init(self): # pylint: disable=no-self-use
         """Test initialization of `SerloDatabase`."""
         SerloDatabase("sqlite:///:memory:")
-
-# ModelTest shall not be tested (see https://stackoverflow.com/a/43353680 )
-del ModelTest
