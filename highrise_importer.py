5b9c98c2eb76797fcf4253f41f3cf54c2e99a7e2
Stephan Kulla <git.mail@kulla.me>
2018-03-31 20:15:50 +0200

    highrise: Parse Highrise API token

--- highrise_importer.py
+++ highrise_importer.py
@@ -1,8 +1,16 @@
 """Imports contact informations from Highrise into a local database."""
 
+import os
+import sys
+
+TOKEN_VARIABLE = "HIGHRISE_API_TOKEN"
+
 def run_script():
     """Executes this script."""
-    pass
+    try:
+        api_token = os.environ[TOKEN_VARIABLE]
+    except KeyError:
+        sys.exit(f"Error: Environment Variable {TOKEN_VARIABLE} not defined.")
 
 if __name__ == "__main__":
     run_script()
