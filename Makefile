00a5c341af7545bc0e64f0e893b47148b638d03e
Stephan Kulla <git.mail@kulla.me>
2018-03-31 20:33:27 +0200

    highrise: Add loading of database

--- Makefile
+++ Makefile
@@ -1,10 +1,12 @@
+ROOT_DIR := $(shell dirname $(abspath $(lastword $(MAKEFILE_LIST))))
+DATABASE := sqlite:///$(ROOT_DIR)/serlo.db
 PYTHON := $(shell if which pyenv > /dev/null; \
                  then echo python ; else echo python3 ; fi)
 
 .PHONY: highrise_import, test
 
 highrise_import:
-	$(PYTHON) highrise_importer.py
+	$(PYTHON) highrise_importer.py '$(DATABASE)'
 
 test:
 	$(PYTHON) -m nose --with-doctest serlo
--- highrise_importer.py
+++ highrise_importer.py
@@ -3,14 +3,23 @@
 import os
 import sys
 
+from serlo.model import SerloDatabase
+
 TOKEN_VARIABLE = "HIGHRISE_API_TOKEN"
 
 def run_script():
     """Executes this script."""
     try:
+        database_file = sys.argv[1]
+    except IndexError:
+        sys.exit(f"Error: No database file specified as first argument.")
+
+    try:
         api_token = os.environ[TOKEN_VARIABLE]
     except KeyError:
         sys.exit(f"Error: Environment Variable {TOKEN_VARIABLE} not defined.")
+
+    database = SerloDatabase(database_file)
 
 if __name__ == "__main__":
     run_script()
